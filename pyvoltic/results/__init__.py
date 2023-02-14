979cd785298f35ce4ebb4444a59fabc26a2a48d7
Leo Loman <leoloman@Leos-MacBook-Pro.local>
2023-02-14 15:22:24 +0000

    refactor results folder

--- pyvoltic/results/__init__.py
+++ pyvoltic/results/__init__.py
@@ -1,3 +1,3 @@
-from .SimulationResults import EBCMResults, DFDResults, NEResults, SRResults
+from ._simulation_results import EBCMResults, DFDResults, NEResults, SRResults
 
 __all__ = ["EBCMResults", "DFDResults", "NEResults", "SRResults"]
