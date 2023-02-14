eb32c49d7b70307df9382dcef1fe475c5cb4adfe
Leo Loman <leoloman@Leos-MacBook-Pro.local>
2023-02-14 15:21:18 +0000

    refactor probability gen folder

--- pyvoltic/probability_generating_functions/__init__.py
+++ pyvoltic/probability_generating_functions/__init__.py
@@ -2,18 +2,18 @@
 The :mod:`pyvoltic.probability_generating_functions` module implements a variety of probabiltiy generating functions, with a poisson, powerlaw and network based funcitons ready to go
 """
 
-from .poisson_pgf import (
+from ._poisson_pgf import (
     poisson_calc_g, 
     poisson_calc_g1, 
     poisson_calc_g2
 )
-from .powerlaw_pgf import (
+from ._powerlaw_pgf import (
     powerlaw_p_vec, 
     powerlaw_calc_g, 
     powerlaw_calc_g1, 
     powerlaw_calc_g2
 )
-from .probability_generating_function import (
+from ._probability_generating_function import (
     get_Pk,
     get_PGF,
     get_PGF_first_derivate,
