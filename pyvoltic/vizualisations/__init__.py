e183598b1faebfc439a593efa8876a9d721fe87e
Leo Loman <leoloman@Leos-MacBook-Pro.local>
2023-02-14 15:27:11 +0000

    refactor viz

--- pyvoltic/vizualisations/__init__.py
+++ pyvoltic/vizualisations/__init__.py
@@ -1,4 +1,4 @@
-from .VizFunctions import NE_multi_cumulative_incidence, SR_multi_cumulative_incidence
+from ._viz_functions import NE_multi_cumulative_incidence, SR_multi_cumulative_incidence
 
 __all__ = ["NE_multi_cumulative_incidence", "SR_multi_cumulative_incidence"]
 
