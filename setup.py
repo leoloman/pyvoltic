9ccb2d0fa4fb3c2f11f4cc22479a0a7183144856
Leo Loman <leoloman@Leos-MacBook-Pro.local>
2023-02-14 11:48:36 +0000

    added find_packages

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name = 'pyvoltic',
       url = 'https://github.com/leoloman/pyvoltic'.
       author = 'Leo Loman',
       author_email = 'leo.loman@outlook.com',
-      packages = ['pyvoltic'],
+      packages = find_packages(where = 'pyvoltic'),
       install_requires = ['numpy', 'math', 'networkx', 'matplotlib', 'scipy'],
       version='0.1',
       license='MIT',
-      description='A framework for the Volz Miller equations',
+      description='A framework for the Edge Based Compartmental Model (Volz Miller) equations',
       long_description=open('README.txt').read()
      )
       
