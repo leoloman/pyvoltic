5b2f366d7629cecc9dca74d33dc4636c9d0b3814
Leo Loman <leoloman@Leos-MacBook-Pro.local>
2023-02-15 12:11:38 +0000

    added functions to be imported by default

--- pyvoltic/__init__.py
+++ pyvoltic/__init__.py
@@ -2,3 +2,35 @@
 __import__("pkg_resources").declare_namespace(__name__)
 
 __version__ = '0.0.1'
+
+from models import SIRNE, SIRSR, EBCM
+
+from probability_generating_functions import (poisson_calc_g, 
+    poisson_calc_g1, 
+    poisson_calc_g2,
+    powerlaw_p_vec, 
+    powerlaw_calc_g, 
+    powerlaw_calc_g1, 
+    powerlaw_calc_g2,
+    get_Pk,
+    get_PGF,
+    get_PGF_first_derivate,
+    get_PGF_second_derivate
+)
+                                              
+                                              
+__all__ = ["SIRNE", 
+           "SIRSR", 
+           "EBCM",
+    "poisson_calc_g", 
+    "poisson_calc_g1", 
+    "poisson_calc_g2",
+    "powerlaw_p_vec", 
+    "powerlaw_calc_g", 
+    "powerlaw_calc_g1", 
+    "powerlaw_calc_g2",
+    "get_Pk",
+    "get_PGF",
+    "get_PGF_first_derivate",
+    "get_PGF_second_derivate"
+]
