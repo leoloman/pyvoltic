990e6a986dadc7bf333f1623341e1987e00fce22
Leo Loman <leoloman@Leos-MacBook-Pro.local>
2023-02-15 12:28:05 +0000

    corrected import

--- pyvoltic/__init__.py
+++ pyvoltic/__init__.py
@@ -3,9 +3,9 @@ __import__("pkg_resources").declare_namespace(__name__)
 
 __version__ = '0.0.1'
 
-from models import SIRNE, SIRSR, EBCM
+from pyvoltic.models import SIRNE, SIRSR, EBCM
 
-from probability_generating_functions import (poisson_calc_g, 
+from pyvoltic.probability_generating_functions import (poisson_calc_g, 
     poisson_calc_g1, 
     poisson_calc_g2,
     powerlaw_p_vec, 
