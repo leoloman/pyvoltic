a22275b83e3dd30e7c0d739c33671df3bce28591
Leo Loman <leoloman@Leos-MacBook-Pro.local>
2023-02-19 01:22:29 +0000

    add doc info

--- pyvoltic/vizualisations/__init__.py
+++ pyvoltic/vizualisations/__init__.py
@@ -1,3 +1,6 @@
+"""
+The :mod:`pyvoltic.vizualisations` module provides a series of functions to compare cumulative incidence across simulation runs, and other useful viz
+"""
 from ._viz_functions import (NE_multi_cumulative_incidence, 
                              SR_multi_cumulative_incidence,
                              EBCM_multi_cumulative_incidence)
