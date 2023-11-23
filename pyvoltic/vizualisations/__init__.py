fcca6867679966934337c357d508c24bdbce4943
Leo Loman <leoloman@Leos-MacBook-Pro.local>
2023-11-23 16:03:08 +0000

    ruff formatting

--- pyvoltic/vizualisations/__init__.py
+++ pyvoltic/vizualisations/__init__.py
@@ -1,11 +1,14 @@
 """
 The :mod:`pyvoltic.vizualisations` module provides a series of functions to compare cumulative incidence across simulation runs, and other useful viz
 """
-from ._viz_functions import (NE_multi_cumulative_incidence, 
-                             SR_multi_cumulative_incidence,
-                             EBCM_multi_cumulative_incidence)
+from ._viz_functions import (
+    EBCM_multi_cumulative_incidence,
+    NE_multi_cumulative_incidence,
+    SR_multi_cumulative_incidence,
+)
 
-__all__ = ["NE_multi_cumulative_incidence", 
-           "SR_multi_cumulative_incidence",
-          "EBCM_multi_cumulative_incidence"]
-
+__all__ = [
+    "NE_multi_cumulative_incidence",
+    "SR_multi_cumulative_incidence",
+    "EBCM_multi_cumulative_incidence",
+]
