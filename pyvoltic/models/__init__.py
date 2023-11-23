e776b4ea732a0a1a48f800cff23d83f541ac24f4
Leo Loman <leoloman@Leos-MacBook-Pro.local>
2023-11-23 16:40:02 +0000

    ruff format

--- pyvoltic/models/__init__.py
+++ pyvoltic/models/__init__.py
@@ -4,10 +4,5 @@ The :mod:`pyvoltic.models` module provides models to spread disease on a heterge
 from ._classic_models import SIRNE, SIRSR
 from ._ebcm import EBCM, MFSHEBCM, DynamicFixedDegree
 from ._ext_models import SEIRSR
-__all__ = ["SIRNE", 
-           "SIRSR",
-           "EBCM",
-           "MFSHEBCM",
-           "DynamicFixedDegree",
-           "SEIRSR"
-          ]
+
+__all__ = ["SIRNE", "SIRSR", "EBCM", "MFSHEBCM", "DynamicFixedDegree", "SEIRSR"]
