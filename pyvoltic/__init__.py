cb0661dfb379d49dbb3232dad2a2bfaae801aead
Leo Loman <leoloman@Leos-MacBook-Pro.local>
2023-11-23 16:38:36 +0000

    ruff format

--- pyvoltic/__init__.py
+++ pyvoltic/__init__.py
@@ -1,36 +1,35 @@
-
 __import__("pkg_resources").declare_namespace(__name__)
 
-__version__ = '0.0.1'
+__version__ = "0.0.1"
 
-from .models import SIRNE, SIRSR, EBCM
-
-from .probability_generating_functions import (poisson_calc_g, 
-    poisson_calc_g1, 
-    poisson_calc_g2,
-    powerlaw_p_vec, 
-    powerlaw_calc_g, 
-    powerlaw_calc_g1, 
-    powerlaw_calc_g2,
-    get_Pk,
+from .models import EBCM, SIRNE, SIRSR
+from .probability_generating_functions import (
     get_PGF,
     get_PGF_first_derivate,
-    get_PGF_second_derivate
+    get_PGF_second_derivate,
+    get_Pk,
+    poisson_calc_g,
+    poisson_calc_g1,
+    poisson_calc_g2,
+    powerlaw_calc_g,
+    powerlaw_calc_g1,
+    powerlaw_calc_g2,
+    powerlaw_p_vec,
 )
-                                              
-                                              
-__all__ = ["SIRNE", 
-           "SIRSR", 
-           "EBCM",
-    "poisson_calc_g", 
-    "poisson_calc_g1", 
+
+__all__ = [
+    "SIRNE",
+    "SIRSR",
+    "EBCM",
+    "poisson_calc_g",
+    "poisson_calc_g1",
     "poisson_calc_g2",
-    "powerlaw_p_vec", 
-    "powerlaw_calc_g", 
-    "powerlaw_calc_g1", 
+    "powerlaw_p_vec",
+    "powerlaw_calc_g",
+    "powerlaw_calc_g1",
     "powerlaw_calc_g2",
     "get_Pk",
     "get_PGF",
     "get_PGF_first_derivate",
-    "get_PGF_second_derivate"
+    "get_PGF_second_derivate",
 ]
